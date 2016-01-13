2c0f2b34a2b17081d0cca6e363d6f728fdcd6b90
CRASHFORT <ludwige55@gmail.com>
2016-01-13 21:27:54 +0100

    For real this time

--- cl_dll/view.cpp
+++ cl_dll/view.cpp
@@ -803,7 +803,7 @@ void V_CalcNormalRefdef ( struct ref_params_s *pparams )
 			pitch += 360;
 
 		// Player pitch is inverted
-		pitch /= 3.0;
+		pitch *= -0.3;
 
 		// Slam local player's pitch value
 		ent->angles[ 0 ] = pitch;
